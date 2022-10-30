0eef058b4436a34523820f70d78412da7579d23d
fajarfadillahh <fajaragustian979@gmail.com>
2022-10-30 19:55:29 +0700

    add dark mode feature

--- js/main.js
+++ js/main.js
@@ -10,3 +10,27 @@ function showMenu(menuId, toggleId) {
   }
 }
 showMenu("header-menu", "header-toggle");
+
+// ===== DARK MODE =====
+// change the icons inside the button based on previous setting
+if (localStorage.getItem("theme", "dark")) {
+  setDarkMode();
+}
+
+// theme toggle button
+function setDarkMode() {
+  var setDark = document.documentElement.classList.toggle("dark");
+  var emoticon = "";
+
+  // if setting via local storage previously
+  if (setDark) {
+    emoticon = "☀️";
+    localStorage.setItem("theme", "dark");
+  } else {
+    emoticon = "🌙";
+    localStorage.removeItem("theme");
+  }
+
+  // put the icons inside the button
+  document.getElementById("theme-toggle").innerHTML = emoticon;
+}
